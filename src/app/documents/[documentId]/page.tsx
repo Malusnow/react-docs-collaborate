ccb6cf007a82b1a2bcd5b8147769193f6464ad46
Malusnow <1721487579@qq.com>
2026-06-30 21:50:12 +0800

    feat: navbar tools

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,4 +1,5 @@
 import { Editor } from "./editor";
+import { Navbar } from "./navbar";
 import { Toolbar } from "./toolbar";
 
 interface DocumentIdPageProps {
@@ -12,8 +13,13 @@ const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-[#fafbfd]">
-      <Toolbar />
-      <Editor />
+      <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#fbfafd] print:hidden">
+        <Navbar />
+        <Toolbar />
+      </div>
+      <div className="pt-[114px] print:pt-0">
+        <Editor />
+      </div>
     </div>
   );
 };
