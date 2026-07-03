440165707c9522ef0ee3ba0773c9e1966a4af840
Malusnow <1721487579@qq.com>
2026-07-03 20:48:27 +0800

    feat: database setup

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+
 import Link from "next/link";
 import { Navbar } from "./navbar";
 import { TemplatesGallery } from "./templates-gallery";
@@ -5,6 +10,15 @@ import { TemplatesGallery } from "./templates-gallery";
 
 
 const Home = () => {
+  const documents = useQuery(api.documents.get);
+
+  if (!documents) {
+    return (
+      <p>
+        loading...
+      </p>
+    )
+  }
   return (
     <div className="min-h-screen flex flex-col">
         <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -12,6 +26,9 @@ const Home = () => {
         </div>
         <div className="mt-16">
           <TemplatesGallery />
+          {documents?.map((doc) => (
+            <span key={doc._id}>{doc.title}</span>
+          ))}
         </div>
     </div>
   );
